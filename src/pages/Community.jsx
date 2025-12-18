ca7cb0c31637447ed8fbf8c2a6252ad1fcf7715c
Mahfuj Alam Rony <mahfujalamrony07@gmail.com>
2025-12-18 23:32:09 +0600

    complate deshboard ui

--- src/pages/Community.jsx
+++ src/pages/Community.jsx
@@ -1,9 +1,63 @@
+import { Heart } from 'lucide-react';
 import React from 'react'
 
 const Community = () => {
+  const [creations, setCreations] = React.useState([]);
+  const [user, setUser] = React.useState(null);
+
+
+  //dummy data for testing
+
+  const dummyCreations = [
+    {
+      content: '/g1.jpg',
+      prompt: 'A beautiful sunset over the mountains',
+      likes: ['user1', 'user2']
+    },
+    {
+      content: '/g2.jpg',
+      prompt: 'A futuristic cityscape at night',
+      likes: ['user2', 'user3', 'user4']
+    },
+    {
+      content: '/g3.jpg',
+      prompt: 'A serene beach with crystal clear water',
+      likes: []
+    }
+  ];
+  React.useEffect(() => {
+    setCreations(dummyCreations);
+  }, []);
   return (
-    <div>
-      Community
+    <div  className='flex-1 h-full flex flex-col gap-4 p-6'>
+      Creations
+      <div className='bg-white h-full w-full rounded-xl overflow-y-scroll'>
+        {creations.length === 0 ? (
+          <div className='h-full flex flex-col justify-center items-center gap-4 text-gray-400'>
+            <p className='text-2xl'>No Creations Yet</p>
+          </div>
+        ) : (
+          creations.map((creation, index) => (
+            <div key={index}
+            className='relative group inline-block px-3 pt-3 w-full sm:max-w-1/2 lg:max-w-1/3'
+            >
+              <img src={creation.content} alt=""
+              className='w-full h-64 object-cover rounded-lg'
+              />
+
+              <div className='absolute bottom-0 top-0 right-0 left-3 flex gap-2 items-end group-hover:justify-between p-3 group-hover:bg-linear-to-b from-transparent to-black/80 text-white rounded-lg'>
+                <p>{creation.prompt}</p>
+                <div className='flex gap-1 items-center'>
+                  <p>{creation.likes.length}</p>
+                  <Heart className={`w-5 h-5 hover:scale-110 cursor-pointer
+                    
+                    ${creation.likes.includes(user?.id) ? 'fill-red-500 text-red-600' : 'text-white'}`}/>
+                </div>
+              </div>
+            </div>
+          ))
+        )}
+      </div>
     </div>
   )
 }
--- src/pages/ReviewResume.jsx
+++ src/pages/ReviewResume.jsx
@@ -1,9 +1,59 @@
+import { EarIcon, Edit, Eraser, EraserIcon, FileText, Hash, Sparkles } from 'lucide-react'
 import React from 'react'
 
 const ReviewResume = () => {
+
+  const [input, setInput] = React.useState('');
+
+  const onSubmit = (e) => {
+    e.preventDefault();
+  } 
+
+
   return (
-    <div>
-      ReviewResume
+    <div className='h-full overflow-y-scroll p-6 flex items-start flex-wrap gap-4 text-slate-700'>
+      <form action=""
+      className='w-full max-w-lg p-4 bg-white rounded-lg border-gray-200'
+      >
+        <div className='flex items-center gap-3'>
+          <Sparkles className='w-6 text-[#4a7aff]' />
+          <h1 className='text-xl font-semibold '>Review Resume</h1>
+        </div>
+
+        <p className='mt-6 text-sm font-medium'>Upload Resume</p>
+
+        <input type="file"
+        accept='application/pdf,application/msword,application/vnd.openxmlformats-officedocument.wordprocessingml.document'
+        className='w-full p-2 px-3 mt-2 outline-none text-sm rounded-md border border-gray-300 text-gray-600'
+        required
+        />
+
+
+        <p className='text-xs text-gray-500 font-light mt-1'>Supports PDF, DOC, and DOCX formats</p>
+        <button
+        className='w-full flex justify-center items-center gap-2 bg-linear-to-r  from-[#f6ab41] to-[#ff4938] text-white px=4 py-2 mt-6 text-sm rounded-lg cursor-pointer'
+        >
+          <FileText className='w-5'/>
+          Review Resume
+        </button>
+
+      </form>
+
+      {/* right side */}
+
+      <div className='w-full max-w-lg p-4 bg-white rounded-lg flex flex-col border border-gray-200 min-h-96'>
+        <div className='flex items-center gap-3'>
+          <EarIcon className='w-5 h-5 text-[#4a7aff]' />
+          <h1 className='text-xl font-semibold '>Analysis Image</h1>
+        </div>
+
+        <div className='flex-1 flex justify-center items-center'>
+          <div className='text-sm flex flex-col items-center gap-5 text-gray-400'> 
+            <FileText className='w-9 h-9' />
+            <p>Upload a resume and click "Review Resume" to get started</p>
+          </div>
+        </div>
+      </div>
     </div>
   )
 }
